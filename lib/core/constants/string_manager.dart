e2034078888bcfcc0e893a1758b874c9e79809f5
Minahil Akhtar <minahilakhtar@Minahils-MacBook-Pro.local>
2024-07-30 11:18:03 +0500

    Update .gitignore and remove ignored files from tracking

--- lib/core/constants/string_manager.dart
+++
@@ -1,5 +0,0 @@
-class StringManager{
-  static String startUpText = 'Get\nready\nto slay\nin\nstyle';
-  static String apiKey = 'vwGrHeGLRH9GUEtH4mDlod0JCh09eFZ9o5BQ3NVL2OcJk8FeJKe4ChkGVWuN';
-  static String imageGenerativeText = 'The try-on image was successfully generated, showcasing the selected product seamlessly integrated into a realistic visual.';
-}
